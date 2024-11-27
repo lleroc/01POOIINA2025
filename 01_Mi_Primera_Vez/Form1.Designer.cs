9779956b09e3c600def8cfac8dc4a383701cb836
lleroc <llleroc@yahoo.es>
2024-11-26 20:22:26 -0500

    Agregar archivos de proyecto.

---
+++ 01_Mi_Primera_Vez/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace _01_Mi_Primera_Vez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Aceptar = new System.Windows.Forms.Button();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Apellido = new System.Windows.Forms.TextBox();
+            this.lst_Estudiantes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_Aceptar
+            // 
+            this.btn_Aceptar.Location = new System.Drawing.Point(204, 364);
+            this.btn_Aceptar.Name = "btn_Aceptar";
+            this.btn_Aceptar.Size = new System.Drawing.Size(166, 85);
+            this.btn_Aceptar.TabIndex = 0;
+            this.btn_Aceptar.Text = "Aceptar";
+            this.btn_Aceptar.UseVisualStyleBackColor = true;
+            this.btn_Aceptar.Click += new System.EventHandler(this.btn_Aceptar_Click);
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(91, 74);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(279, 34);
+            this.txt_Nombre.TabIndex = 1;
+            this.txt_Nombre.TextChanged += new System.EventHandler(this.txt_Nombre_TextChanged);
+            this.txt_Nombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Nombre_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese su Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ingrese su Apellido";
+            // 
+            // txt_Apellido
+            // 
+            this.txt_Apellido.Location = new System.Drawing.Point(91, 154);
+            this.txt_Apellido.Name = "txt_Apellido";
+            this.txt_Apellido.Size = new System.Drawing.Size(279, 34);
+            this.txt_Apellido.TabIndex = 3;
+            // 
+            // lst_Estudiantes
+            // 
+            this.lst_Estudiantes.FormattingEnabled = true;
+            this.lst_Estudiantes.ItemHeight = 29;
+            this.lst_Estudiantes.Location = new System.Drawing.Point(424, 74);
+            this.lst_Estudiantes.Name = "lst_Estudiantes";
+            this.lst_Estudiantes.Size = new System.Drawing.Size(219, 149);
+            this.lst_Estudiantes.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(752, 629);
+            this.Controls.Add(this.lst_Estudiantes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Apellido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.btn_Aceptar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Aceptar;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Apellido;
+        private System.Windows.Forms.ListBox lst_Estudiantes;
+    }
+}
+
